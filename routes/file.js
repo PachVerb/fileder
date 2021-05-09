7470cd560ff2b0bee9cf8b44b924487e9b3e5f10
PachVerb <1506262681@qq.com>
2021-05-10 01:54:24 +0800

    feat: 目录创建

--- routes/file.js
+++ routes/file.js
@@ -1,34 +1,56 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const fs = require('fs')
-const createDir = require('../utils/creatDir')
-const root = '/Users/ousan/desktop/test'
-router.get('/list', async (req, res) => {
+const fs = require("fs");
+const createDir = require("../utils/creatDir");
+const root = "/Users/ousan/desktop/test";
+router.get("/list", async (req, res) => {
   // source root
-    let file = null;
-    await fs.stat(root, {}, (err, path) => {
+  let file = null;
+  await fs.stat(root, {}, (err, path) => {
+    if (err) throw err;
+    file = path;
+  });
+  if (file) {
+    await fs.mkdir(path, { recursive: false }, (err) => {
       if (err) throw err;
-      file = path;
-    })
-    if (file) {
-      await fs.mkdir(path , { recursive: false }, (err) => {
-        if (err) throw err;
-      })
-    }
-    await fs.readdir(root ,{withFileTypes: true}, (err, files) => {
-      if(err) return res.send({ code: 1, msg: '服务器出了点故障'})
-      return res.send({code: 0, msg: createDir(files)})
-    })
-})
+    });
+  }
+  await fs.readdir(root, { withFileTypes: true }, (err, files) => {
+    if (err) return res.send({ code: 1, msg: "服务器出了点故障" });
+    return res.send({ code: 0, msg: createDir(files) });
+  });
+});
 
-router.post('/getcontent', async (req, res) => {
-  const { path } = req.body
-  const fullPath = root + path.substring(path.indexOf('/') + 1, path.length) 
+router.post("/getcontent", async (req, res) => {
+  const { path } = req.body;
+  const fullPath = root + path.substring(path.indexOf("/") + 1, path.length);
 
-  await fs.readdir(fullPath, {withFileTypes: true}, (err, files) => {
-    if(err) return res.send({ code: 1, msg: '服务器出了点故障' + err})
-    return res.send({code: 0, msg: createDir(files)})
-  })
-})
+  await fs.readdir(fullPath, { withFileTypes: true }, (err, files) => {
+    if (err) return res.send({ code: 1, msg: "服务器出了点故障" + err });
+    return res.send({ code: 0, msg: createDir(files) });
+  });
+});
 
-module.exports = router
+router.post("/mkdir", async (req, res) => {
+  const { path, mkdir } = req.body;
+  const fullPath = root + path.substring(path.indexOf("/") + 1, path.length);
+  let notexit = false;
+  const data = await fs.promises.readdir(fullPath, { withFileTypes: true });
+  if (data.find((val) => val === mkdir)) {
+    res.send({
+      code: 1,
+      msg: "目录已存在",
+    });
+  } else {
+    notexit = true;
+  }
+  if (notexit) {
+    await fs.promises.mkdir(fullPath + "/" + mkdir, { recursive: false });
+    res.send({
+      code: 0,
+      msg: "创建成功",
+    });
+  }
+});
+
+module.exports = router;
--- utils/creatDir.js
+++ utils/creatDir.js
@@ -1,22 +1,23 @@
-const isDir = require('./isDir')
-let root = []
+const isDir = require("./isDir");
+const guid = require("./GUID.js");
+let root = [];
 function createDir(files) {
   if (!files) return;
-  root = []
+  root = [];
   for (let i = 0; i < files.length; i++) {
-    const single = {}
+    const single = {};
 
     if (isDir(files[i])) {
       single.title = files[i].name;
-      single.key = files[i].name;
-      single.children = []
-      single.type = 1
-      root.push(single)
+      single.key = guid();
+      single.children = [];
+      single.type = 1;
+      root.push(single);
     } else {
       single.title = files[i].name;
-      single.key = files[i].name;
-      single.type = 0
-      root.push(single)
+      single.key = guid();
+      single.type = 0;
+      root.push(single);
     }
   }
 
